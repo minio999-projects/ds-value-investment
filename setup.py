8b9a7c5827331dc1835c544073de4425ba242c6b
minio999 <minkowskidominik03@gmail.com>
2022-08-07 13:56:22 +0200

    setup requirements.txt

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open("LICENSE") as file:
     license = file.read() 
 
+with open('requirements.txt') as file:
+    required = file.read().splitlines()
+
 setup(
     name="app",
     version='0.1.0',
@@ -14,4 +17,5 @@ setup(
     include_package_data=True,
     package_dir={"":"src"},
     packages=find_packages(where='src'),
+    install_requires=required,
 )
